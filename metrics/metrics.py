5c576aa1a5bc4807f3dc73dcf430b4787542257d
student-t <student-t@student-ts-MacBook-Pro.local>
2019-06-05 00:09:17 +0800

    added perplexity metrics

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,10 +1,11 @@
-def evaluate_ppl(total_loss: float, n_predict_word: int):
+import numpy as np
+
+
+def evaluate_ppl(total_loss: float, n_predict_words: int):
     """ Calculate perplexity
     Args:
         n_predict_word (int): The total number of words to predict
-        total_loss (float): The
-
-
-
-
-    """"
+        total_loss (float): The total loss of whole set
+    """
+    ppl = np.exp(total_loss / n_predict_words)
+    return ppl
